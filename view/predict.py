caa1393f05a55d969281c8131df15b3ebdf97d1b
Muhammad Aidil Mirza <maidilmirza@gmail.com>
2025-05-15 15:45:13 +0800

    Selesai dashboard cluster

--- view/predict.py
+++ view/predict.py
@@ -10,6 +10,7 @@ data_cluster = data.load_cluster_data()
 data_pca = data.load_pca_cluster_data()
 
 
+
 def cluster_card(df):
     st.markdown(
         """
@@ -34,7 +35,7 @@ def cluster_card(df):
             border-radius: 10px;
             padding: 10px;
             color: #EC0A0B !important;
-            border: 2px solid #EC0A0B !important;,
+            border: 4px solid #EC0A0B !important;,
             border-color: #EC0A0B
         }
 
@@ -78,6 +79,8 @@ def cluster_card(df):
     cluster3 = df[df['Clusters'] == 3]
     jumlah_cluster3 = cluster3['Clusters'].count()
 
+    st.subheader('Informasi singkat per Kluster')
+    st.markdown('\n')
     col1, col2, col3, col4 = st.columns(4)
 
     with col1:
@@ -114,8 +117,70 @@ def cluster_comparison(df):
                 color_continuous_scale=px.colors.qualitative.Set1)
     st.plotly_chart(fig, use_container_width=True, style={'border': '1px solid black'})
     
+    st.markdown('\n')
+    st.subheader("Statistik Tiap Cluster")
+    st.markdown('\n')
+    avg_per_cluster = df.groupby('Clusters')['Avg_Harga'].mean().reset_index()
+    # Membuat bar chart
+    fig_harga = px.bar(avg_per_cluster, x='Clusters', y='Avg_Harga',
+                labels={'Avg_Harga': 'Rata-rata Total Harga yang dibeli', 'Clusters': 'Cluster'},
+                title='Rata-rata Total Harga yang Dibeli per Cluster')
+    st.plotly_chart(fig_harga, use_container_width=True)
+    st.markdown('\n')
+    col1, col2 = st.columns(2)
+    with col1:
+        avg_per_cluster = df.groupby('Clusters')['Total_Pembelian'].mean().reset_index()
+        # Membuat bar chart
+        fig_pembelian = px.bar(avg_per_cluster, x='Clusters', y='Total_Pembelian',
+                    labels={'Total_Pembelian': 'Rata-rata Total Pembelian', 'Clusters': 'Cluster'},
+                    title='Rata-rata Total Pembelian per Cluster')
+
+        st.plotly_chart(fig_pembelian, use_container_width=True)
+    with col2:
+        avg_per_cluster = df.groupby('Clusters')['Total_Quantity'].mean().reset_index()
+        # Membuat bar chart
+        fig_kuantitas = px.bar(avg_per_cluster, x='Clusters', y='Total_Quantity',
+                    labels={'Total_Quantity': 'Rata-rata Total Item yang dibeli', 'Clusters': 'Cluster'},
+                    title='Rata-rata Total Item yang dibeli per Cluster')
+
+        st.plotly_chart(fig_kuantitas, use_container_width=True)
+    st.markdown('\n')
+    col3, col4 = st.columns(2)
+    with col3:
+        avg_per_cluster = df.groupby('Clusters')['Produk_Unik'].mean().reset_index()
+        # Membuat bar chart
+        fig = px.bar(avg_per_cluster, x='Clusters', y='Produk_Unik',
+                    labels={'Produk_Unik': 'Rata-rata Total Produk yang dibeli', 'Clusters': 'Cluster'},
+                    title='Rata-rata Total Pembelian per Cluster')
+        st.plotly_chart(fig, use_container_width=True)
+    with col4:
+        avg_per_cluster = df.groupby('Clusters')['Kategori_Unik'].mean().reset_index()
+        # Membuat bar chart
+        fig = px.bar(avg_per_cluster, x='Clusters', y='Kategori_Unik',
+                    labels={'Kategori_Unik': 'Rata-rata Total Kategori yang dibeli', 'Clusters': 'Cluster'},
+                    title='Rata-rata Total Kategori yang dibeli per Cluster')
+        st.plotly_chart(fig, use_container_width=True)
+    st.markdown("---")
+
 def main():
+    st.markdown(
+        """
+        <style>
+        [data-testid="stMetricValue"] {
+            font-size: 25px;
+            font-weight: bold;
+        }
+
+        .e1kosxz23 {
+            color: #FFFFFFFF !important;
+        }
+        """,
+        unsafe_allow_html=True,
+    )
     st.title("Clustering :mag:")
+    st.markdown("\n")
+    with st.expander("Lihat Dataset"):
+        st.dataframe(data_cluster)
     st.markdown("\n")
     plot_cluster(data_pca)
     cluster_card(data_cluster)
